8d1677074caaf99fcb78b608bf086940cc0e94e7
Oscar Liemena <oscarliemena@gmail.com>
2024-04-15 20:21:14 +1000

    Fixed exports

--- app.js
+++ app.js
@@ -3,83 +3,29 @@ import express from 'express';
 import mongoose from 'mongoose';
 import fs from 'fs';
 import swaggerUi from 'swagger-ui-express';
-import User from './models/user.model.js';
+import userRoute from './routes/user.route.js';
 
-import { executeUsersCrudOperations } from './users.js';
-
+//initialisation
 config();
 const uri = process.env.DB_URI;
 const port = process.env.PORT || 3000;
 const swaggerJson = JSON.parse(fs.readFileSync('./swagger.json'));
 const app = express();
 
+//middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
+//routes
 app.use('/api-docs/', swaggerUi.serve, swaggerUi.setup(swaggerJson));
+app.use('/api/users/', userRoute);
 
+//hello
 app.get('/', (req,res) => {
     res.send("Hello from Node API");
 });
 
-app.get('/api/users', async (req,res) => {
-    try {
-        const users = await User.find({});
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.get('/api/user/:id', async (req,res) => {
-    try {
-        const { id } = req.params;
-        const user = await User.findById(id);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.post('/api/users', async (req,res) => {
-    try {
-        const user = await User.create(req.body);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.put('/api/user/:id', async (req,res) => {
-    try {
-        const { id } = req.params;
-        const user = await User.findByIdAndUpdate(id, req.body);
-
-        if(!user) {
-            return res.status(404).json({message: "User not found"});
-        }
-
-        const updatedUser = await User.findById(id);
-        res.status(200).json(updatedUser);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.delete('/api/user/:id', async (req,res) => {
-    try {
-        const { id } = req.params;
-        const user = await User.findByIdAndDelete(id);
-
-        if(!user) {
-            return res.status(404).json({message: "User not found"});
-        }
-
-        res.status(200).json({message: "User deleted successfully"});
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
+//db connection
 mongoose.connect(uri)
 .then(() => {
     console.log('Connected to database');
@@ -90,5 +36,3 @@ mongoose.connect(uri)
 .catch(() => {
     console.log('Connection failed');
 });
-
-// await executeUsersCrudOperations();
