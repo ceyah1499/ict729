9e563654b8ca89578c772ef88d06567f96c42d96
Oscar Liemena <oscarliemena@gmail.com>
2024-04-15 17:42:46 +1000

    Delete user endpoint

--- app.js
+++ app.js
@@ -65,6 +65,21 @@ app.put('/api/user/:id', async (req,res) => {
     }
 });
 
+app.delete('/api/user/:id', async (req,res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndDelete(id);
+
+        if(!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+
+        res.status(200).json({message: "User deleted successfully"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
 mongoose.connect(uri)
 .then(() => {
     console.log('Connected to database');
